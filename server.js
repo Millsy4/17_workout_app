97071e0153fd61103c9353de0a42d3ebe0941776
Christian Mills <millsy4@hotmail.com>
2021-01-23 22:31:56 -0600

    have everything working just need to do the mongoDB atlas stuff

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndMondify: false,
+  useFindAndModify: false,
 });
 
 app.use(htmlRoutes);
